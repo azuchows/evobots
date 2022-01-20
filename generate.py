85f72411c65f328d3ccf6136d50f4333e67b8125
Adam Zuchowski <adam.zuchowski@uvm.edu>
2022-01-19 21:48:28 -0500

    Absolutely Relative Coordinates

--- generate.py
+++ generate.py
@@ -10,8 +10,13 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
-
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos = [0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link2", pos = [0,0,0.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0,0.5,0.5])
+    pyrosim.Send_Cube(name="Link3", pos = [0,0.5,0], size=[1,1,1])
     pyrosim.End()
 
 Create_World()

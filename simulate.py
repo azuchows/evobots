788b14d61df5455567733ad04604db9b801886ff
Adam Zuchowski <adam.zuchowski@uvm.edu>
2022-01-20 20:13:44 -0500

    Single sensor with console output

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet_data
 import pybullet as p
 import time
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -11,9 +12,12 @@ p.loadSDF("world.sdf")
 
 robotId = p.loadURDF("body.urdf")
 
+pyrosim.Prepare_To_Simulate(2)
+
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
     time.sleep(1/60)
-    print(i)
 
 p.disconnect()

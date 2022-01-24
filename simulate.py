331eb29258228fffeec41d7414630ca780b9f449
Adam Zuchowski <adam.zuchowski@uvm.edu>
2022-01-24 09:20:51 -0500

    Saving sensor data with numpy

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet_data
 import pybullet as p
 import time
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,10 +15,14 @@ robotId = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(2)
 
+backLegSensorValues = np.zeros(1000)
+
 for i in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
     time.sleep(1/60)
 
 p.disconnect()
+
+print(backLegSensorValues)

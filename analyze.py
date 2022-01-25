e8d51b7dc405322122c05f90b554b5ad491411de
azuchows <adam.zuchowski@uvm.edu>
2022-01-25 10:23:00 -0500

    Sinusoidal Swaying

--- analyze.py
+++ analyze.py
@@ -1,11 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
-backLegSensorValues = np.load("C:/Users/zucho/Desktop/UVM/CS206/data/BackLegSensorData.npy")
+#backLegSensorValues = np.load("C:/Users/zucho/Desktop/UVM/CS206/data/BackLegSensorData.npy")
 
-frontLegSensorValues = np.load('C:/Users/zucho/Desktop/UVM/CS206/data/frontLegSensorData.npy')
+#frontLegSensorValues = np.load('C:/Users/zucho/Desktop/UVM/CS206/data/frontLegSensorData.npy')
 
-pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
-pyplot.plot(frontLegSensorValues, label = "Front Leg")
-pyplot.legend()
-pyplot.show()
+#plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
+#plt.plot(frontLegSensorValues, label = "Front Leg")
+#plt.legend()
+
+targetAngles = np.load('C:/Users/zucho/Desktop/UVM/CS206/data/targetAngles.npy')
+plt.plot(targetAngles)
+plt.show()
--- simulate.py
+++ simulate.py
@@ -3,8 +3,9 @@ import pybullet as p
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
-import math
 import random
+
+STEPS = 1000
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -17,17 +18,21 @@ robot = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robot)
 
-backLegSensorValues = np.zeros(1000)
-frontLegSensorValues = np.zeros(1000)
+backLegSensorValues = np.zeros(STEPS)
+frontLegSensorValues = np.zeros(STEPS)
 
-for i in range(1000):
+targetAngles = np.pi/4 * np.sin(np.linspace(-np.pi, np.pi, STEPS))
+
+#np.save('C:/Users/zucho/Desktop/UVM/CS206/data/targetAngles', targetAngles)
+
+for i in range(STEPS):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-math.pi/2, math.pi/2), maxForce = 30)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce = 30)
 
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform(-math.pi/2, math.pi/2), maxForce = 30)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = b'Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce = 30)
 
     time.sleep(1/60)
 

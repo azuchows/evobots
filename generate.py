a6ce37746378811860209e061e90b5ff00d634dd
azuchows <adam.zuchowski@uvm.edu>
2022-02-01 11:32:50 -0500

    Adding a neuron

--- generate.py
+++ generate.py
@@ -8,6 +8,10 @@ def Create_World():
     pyrosim.End()
 
 def Create_Robot():
+    Generate_Body()
+    Generate_Brain()
+
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
 
     pyrosim.Send_Cube(name = "Torso", pos = [1.5,0,1.5] , size = [1,1,1])
@@ -19,6 +23,13 @@ def Create_Robot():
     pyrosim.Send_Joint(name = "Torso_FrontLeg", parent = "Torso", child = "FrontLeg", type = "revolute", position = [2,0,1])
 
     pyrosim.Send_Cube(name = "FrontLeg", pos = [0.5, 0, -0.5], size = [1,1,1])
+
+    pyrosim.End()
+
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
 
     pyrosim.End()
 

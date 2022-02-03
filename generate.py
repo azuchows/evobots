1933d15ba88343ee184a5fa0ef8e79192ac7ebb0
azuchows <adam.zuchowski@uvm.edu>
2022-02-03 13:38:34 -0500

    Random weights for synapses

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -36,12 +37,9 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name = 3, jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4, jointName = "Torso_FrontLeg")
 
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = -1)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = -1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 4, weight = -0.5)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 3, weight = 1)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 1.0)
+    for i in [0, 1, 2]:
+        for j in [3, 4]:
+            pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j, weight = random.uniform(-1,1))
 
     pyrosim.End()
 

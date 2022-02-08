24c49a0517995bd5b9a1396b37b8f7f9f0b66464
azuchows <adam.zuchowski@uvm.edu>
2022-02-08 13:04:48 -0500

    Removing unused variables

--- constants.py
+++ constants.py
@@ -1,20 +1,16 @@
 import numpy as np
 
-amplitude = np.pi/4
-frequency = 10
-offset = 0
-
 maxForce = 30
 
-sleepInt = 1/60
+sleepInt = 1/120
 
 STEPS = 1000
 
 gravity = -9.8
 
-numberOfGenerations = 10
+numberOfGenerations = 1
 
-populationSize = 10
+populationSize = 1
 
 numSensorNeurons = 4
 numMotorNeurons = 8
